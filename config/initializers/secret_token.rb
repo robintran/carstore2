7266b0a480400f7f5f3c351b69764b5c1ffe736c
Duy Tran <duytran_it@yahoo.com.vn>
2013-01-10 16:34:48 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carstore2::Application.config.secret_token = 'af0dc2243f764a0ed121baacdd4b135e228b5db790ed70c3767d4fc19966adaef9d356759ac80cc0b2034c20634f544eed087e597d88febecba7358b29e097ac'
