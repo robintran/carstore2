519edf09d57bbb914dc07997e250de0c7979cf4b
Duy Tran <duytran_it@yahoo.com.vn>
2013-01-10 17:49:21 +0700

    Configure model Category's attributes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
 	has_and_belongs_to_many :products
 	acts_as_nested_set
-  attr_accessible :depth, :lft, :link, :name, :rgt
+  attr_accessible :depth, :lft, :link, :name, :rgt, :parent
 end
