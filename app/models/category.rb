da5b5f5046d9385e8b2d70a629d77810196f5b43
Duy Tran <duytran_it@yahoo.com.vn>
2013-01-11 10:59:11 +0700

    Add validates

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
 	has_and_belongs_to_many :products
 	acts_as_nested_set
   attr_accessible :depth, :lft, :link, :name, :rgt, :parent
+  validates :name, :presence => true, :uniqueness => true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
 	has_and_belongs_to_many :categories
   attr_accessible :name, :id
+  validates :name, :presence => true
+  validates :id, :presence => true, :uniqueness => true
 end
